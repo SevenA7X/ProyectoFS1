6ae62404d930cf8b0a37300eb6574ccc820c24bc
Matias <matia.penas@duocuc.cl>
2026-05-11 18:06:39 -0400

    Ahora si, añadido el DTO

--- Compras/src/main/java/BibliotecaDigital/Compras/dto/ComprasDTO.java
+++ Compras/src/main/java/BibliotecaDigital/Compras/dto/ComprasDTO.java
@@ -1,5 +1,25 @@
 package BibliotecaDigital.Compras.dto;
 
-public class ComprasDTO {
+import java.time.LocalDate;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PastOrPresent;
+import lombok.Data;
 
+@Data
+public class ComprasDTO {
+    private Long compraID;
+
+    @NotNull(message = "El ID de usuario es obligatorio")
+    private Long usuarioID;
+
+    @NotNull(message = "El ID del videojuego es obligatorio")
+    private Long videojuegoID;
+
+    @NotNull(message = "La fecha de compra no puede ser nula")
+    @PastOrPresent(message = "La fecha de compra no puede ser futura") 
+    private LocalDate fecha_compra;
+
+    @NotBlank(message = "El estado de la orden no puede estar vacío")
+    private String estado_orden;
 }
