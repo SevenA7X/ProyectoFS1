a50f955f663ec7a056441b897f3799c4d272e612
Matias <matia.penas@duocuc.cl>
2026-05-11 18:01:13 -0400

    Cree el DTO para el microservicio compras donde movi las validaciones

--- Compras/src/main/java/BibliotecaDigital/Compras/Modelo/Compra.java
+++ Compras/src/main/java/BibliotecaDigital/Compras/Modelo/Compra.java
@@ -1,16 +1,12 @@
 package BibliotecaDigital.Compras.Modelo;
 
 import java.time.LocalDate;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -26,20 +22,15 @@ public class Compra {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long compraID;
     
-    @NotNull(message = "El ID de usuario es obligatorio")
     @Column(nullable= false)
     private Long usuarioID;
 
-    @NotNull(message = "El ID del videojuego es obligatorio")
     @Column(nullable= false)
     private Long videojuegoID;
 
-    @NotNull(message = "La fecha de compra no puede ser nula")
-    @PastOrPresent(message = "La fecha de compra no puede ser futura") 
     @Column(nullable= false)
     private LocalDate fecha_compra;
 
-    @NotBlank(message = "El estado de la orden no puede estar vacío")
     @Column(nullable= false)
     private String estado_orden;
 }
---
+++ Compras/src/main/java/BibliotecaDigital/Compras/dto/ComprasDTO.java
@@ -0,0 +1,5 @@
+package BibliotecaDigital.Compras.dto;
+
+public class ComprasDTO {
+
+}
