8368a4822acfb12c9525e9acd7ee83a82e24079d
Matias <matia.penas@duocuc.cl>
2026-05-11 18:27:30 -0400

    Cree el DTO para el microservicio licenciasydescargas donde movi las validaciones

--- LicenciasYDescargas/src/main/java/BibliotecaJuegos/LicenciasYDescargas/Modelo/Licencia.java
+++ LicenciasYDescargas/src/main/java/BibliotecaJuegos/LicenciasYDescargas/Modelo/Licencia.java
@@ -1,18 +1,7 @@
 package BibliotecaJuegos.LicenciasYDescargas.Modelo;
 
-
-
 import java.time.LocalDate;
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PastOrPresent;
-import jakarta.validation.constraints.Positive;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,25 +11,18 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Table(name = "db_licencias")
 @Entity
-
 public class Licencia {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long licenciaID;
 
-    @NotNull(message = "El ID de usuario es obligatorio")
-    @Positive(message = "El ID de usuario debe ser un número positivo")
     @Column(nullable=false)
     private Long usuarioID;
     
-    @NotNull(message = "El ID del videojuego es obligatorio")
-    @Positive(message = "El ID del videojuego debe ser un número positivo")
     @Column(nullable=false)
     private Long videojuegoID;
 
-    @NotNull(message = "La fecha de emisión de la licencia es obligatoria")
-    @PastOrPresent(message = "La fecha de la licencia no puede ser una fecha futura")
     @Column(nullable=false)
     private LocalDate fecha;
 }
