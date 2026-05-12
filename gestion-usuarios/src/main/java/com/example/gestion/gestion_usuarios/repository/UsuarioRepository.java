2720a2de17a950246cf0810b819f6ec04d793c14
HypershadiX <ja.astudillob@duocuc.cl>
2026-05-11 21:25:57 -0400

    añadir el microservicio de gestión-usuarios

--- gestion-usuarios/src/main/java/com/example/gestion/gestion_usuarios/repository/UsuarioRepository.java
+++ gestion-usuarios/src/main/java/com/example/gestion/gestion_usuarios/repository/UsuarioRepository.java
@@ -2,9 +2,11 @@ package com.example.gestion.gestion_usuarios.repository;
 
 import com.example.gestion.gestion_usuarios.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+    // JPQL Personalizado
+    @Query("SELECT u FROM Usuario u WHERE u.email = :email")
+    Usuario findByEmailPersonalizado(@Param("email") String email);
 }
