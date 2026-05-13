ae015d873be8c6bd3a929ba9b6c02d350b0c4424
Mario Antonio Quintero <ma.quinteroa@duocuc.cl>
2026-05-13 17:16:56 -0400

    integrando fallback para que el microservicio moderacion no colapse ante caidas

--- moderacion/src/main/java/com/proyectofs1/moderacion/client/ResenaFeignClient.java
+++ moderacion/src/main/java/com/proyectofs1/moderacion/client/ResenaFeignClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "resena-service", url = "http://localhost:8082/api/v1/resenas")
+@FeignClient(name = "ms-resena", url = "http://localhost:8082/api/v1/resenas", fallback = ResenaFeignClientFallback.class)
 public interface ResenaFeignClient {
     @GetMapping("/{id}")
     Object obtenerResenaPorId(@PathVariable("id") Long id);
