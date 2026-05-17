0b684fa0d1a99e9ddaf3eab7875ac695869dc2cf
Matias <matia.penas@duocuc.cl>
2026-05-17 15:06:26 -0400

    Solucionado errores con el archivo pom.xml

--- HistorialCompras/src/main/java/BibliotecaJuegos/HistorialCompras/HistorialComprasApplication.java
+++ HistorialCompras/src/main/java/BibliotecaJuegos/HistorialCompras/HistorialComprasApplication.java
@@ -2,8 +2,10 @@ package BibliotecaJuegos.HistorialCompras;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class HistorialComprasApplication {
 
 	public static void main(String[] args) {
--- HistorialCompras/src/main/java/BibliotecaJuegos/HistorialCompras/client/FeignClientConfig.java
+++ HistorialCompras/src/main/java/BibliotecaJuegos/HistorialCompras/client/FeignClientConfig.java
@@ -1,22 +1,14 @@
 package BibliotecaJuegos.HistorialCompras.client;
 
-import feign.Feign;
-import feign.jackson.JacksonDecoder;
-import feign.jackson.JacksonEncoder;
-import feign.slf4j.Slf4jLogger;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import feign.Logger; // 👈 Este import nativo de feign se mantiene perfectamente
 
 @Configuration
 public class FeignClientConfig {
 
     @Bean
-    public ComprasFeignClient compraClient() {
-        return Feign.builder()
-                .encoder(new JacksonEncoder()) 
-                .decoder(new JacksonDecoder()) 
-                .logger(new Slf4jLogger(ComprasFeignClient.class))
-                .logLevel(feign.Logger.Level.FULL)
-                .target(ComprasFeignClient.class, "http://localhost:8085");
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
