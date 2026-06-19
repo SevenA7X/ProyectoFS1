7ce371ffeaa4a09d4eb1f99004164a61d37f16fd
HypershadiX <ja.astudillob@duocuc.cl>
2026-06-19 16:55:37 -0400

    eliminación de carpeta errónea

--- gestion-usuarios/target/classes/import.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO usuarios (id, nombre_usuario, email, password, rol) VALUES (1, 'Juan Pérez', 'juanperez@gmail.com', '123456', 'CLIENTE');
-INSERT INTO usuarios (id, nombre_usuario, email, password, rol) VALUES (2, 'María Valenzuela', 'mariavalen@gmail.com', '654321', 'CLIENTE');
-INSERT INTO usuarios (id, nombre_usuario, email, password, rol) VALUES (3, 'Rodrigo Fernández', 'rodrifernan@gmail.com', '162534', 'CLIENTE');
-INSERT INTO usuarios (id, nombre_usuario, email, password, rol) VALUES (4, 'Mauricio Rojas', 'maurirojas@gmail.com', '615243', 'CLIENTE');
-INSERT INTO usuarios (id, nombre_usuario, email, password, rol) VALUES (5, 'Leonardo Gutierrez', 'leoguti@gmail.com', '651243', 'CLIENTE');
